245ec403e24b66cf65254db6da1fa26f4027fabb
Adarsh <adarshshanu3@gmail.com>
2024-07-05 16:46:12 +0530

    some files removed and tailwind css and typescript are configured successfully

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TopButtons from './components/TopButtons';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br  h-fit shadow-xl shadow-gray-400">
+      <TopButtons />
     </div>
   );
 }
---
+++ src/components/TopButtons.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TopButtons = () => {
+    return (
+        <div>
+            Button
+        </div>
+    )
+}
+
+export default TopButtons
